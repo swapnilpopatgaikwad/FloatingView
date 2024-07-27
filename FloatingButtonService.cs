220ed0a8e1634d40b533fb3efd7686d67455d0d5
SWAPNIL GAIKWAD <swapnilpopatgaikwad@gmail.com>
2024-07-27 16:14:17 +0530

    This implementation will allow you to have a floating button that can be dragged and moved to the sides of the screen

--- FloatingButtonService.cs
+++ FloatingButtonService.cs
@@ -10,17 +10,22 @@ using System;
 namespace FloatingView
 {
     [Service]
-    public class FloatingButtonService : Service
+    public class FloatingButtonService : Service, View.IOnTouchListener
     {
         private WindowManagerLayoutParams layoutParams;
         private IWindowManager windowManager;
         private View floatingView;
+        private float initialTouchX;
+        private float initialTouchY;
+        private int initialX;
+        private int initialY;
 
         public override void OnCreate()
         {
             base.OnCreate();
 
             floatingView = LayoutInflater.From(this).Inflate(Resource.Layout.floating_button, null);
+            floatingView.SetOnTouchListener(this);
 
             layoutParams = new WindowManagerLayoutParams(
                 WindowManagerLayoutParams.WrapContent,
@@ -29,7 +34,7 @@ namespace FloatingView
                 WindowManagerFlags.NotFocusable,
                 Format.Translucent)
             {
-                Gravity = GravityFlags.Center | GravityFlags.Center,
+                Gravity = GravityFlags.Top | GravityFlags.Left,
                 X = 0,
                 Y = 100
             };
@@ -58,6 +63,39 @@ namespace FloatingView
         {
             Toast.MakeText(this, "Emergency Button Clicked", ToastLength.Short).Show();
             // Handle the button click action here
+        }
+
+        public bool OnTouch(View v, MotionEvent e)
+        {
+            switch (e.Action)
+            {
+                case MotionEventActions.Down:
+                    initialX = layoutParams.X;
+                    initialY = layoutParams.Y;
+                    initialTouchX = e.RawX;
+                    initialTouchY = e.RawY;
+                    return true;
+
+                case MotionEventActions.Up:
+                    // If the button is dragged to the edge, stick it there
+                    if (layoutParams.X < (windowManager.DefaultDisplay.Width / 2))
+                    {
+                        layoutParams.X = 0;
+                    }
+                    else
+                    {
+                        layoutParams.X = windowManager.DefaultDisplay.Width;
+                    }
+                    windowManager.UpdateViewLayout(floatingView, layoutParams);
+                    return true;
+
+                case MotionEventActions.Move:
+                    layoutParams.X = initialX + (int)(e.RawX - initialTouchX);
+                    layoutParams.Y = initialY + (int)(e.RawY - initialTouchY);
+                    windowManager.UpdateViewLayout(floatingView, layoutParams);
+                    return true;
+            }
+            return false;
         }
     }
 }
